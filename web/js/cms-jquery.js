877837d5490f104bd02e97ddb1c4f327ae69b7e3
JL <johnnie-l@Live.ca>
2016-03-24 18:12:22 -0700

    Edited javascript a bit

--- web/js/cms-jquery.js
+++ web/js/cms-jquery.js
@@ -3,5 +3,16 @@ $(document).ready(function(){
 	$(document).on('click', '#delete_modal', (function(){
 		$($(this).attr("data-target")).appendTo("body");
 	}));
+	
+	$(document).on('click', '#conf_expand', (function(){
+		x=document.getElementById('expand_button');
+		if (x.innerText == 'expand_more'){
+			x.innerText='expand_less';
+		}
+		else {
+			x.innerText='expand_more';
+		}
+		
+	}));
 
 });
